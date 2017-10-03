3d5f7b1189ff4c3c9afca485c2e490e31a4c1da5
RumbleFrog <xxlosttechxx@gmail.com>
2017-10-03 17:40:46 -0400

    Primary class
    Signed-off-by: RumbleFrog <xxlosttechxx@gmail.com>

---
+++ src/MoneyConverter.java
@@ -0,0 +1,13 @@
+/**
+ * @author Z
+ * @since 3 October 2017
+ */
+
+public class MoneyConverter {
+    static int iQuarters, iDimes, iNickels, iPennies;
+
+    public static void main(String[] args)
+    {
+
+    }
+}
