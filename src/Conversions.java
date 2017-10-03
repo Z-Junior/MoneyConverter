d75645d1c193ab31b8d2a9e2df653b64b14a42ad
RumbleFrog <xxlosttechxx@gmail.com>
2017-10-03 17:56:35 -0400

    Declaration of base conversions
    Signed-off-by: RumbleFrog <xxlosttechxx@gmail.com>

---
+++ src/Conversions.java
@@ -0,0 +1,21 @@
+public class Conversions
+{
+    private int iQuarters, iDimes, iNickels, iPennies;
+    final double QUARTER_WORTH = 0.25, DIME_WORTH = 0.1, NICKEL_WORTH = 0.05, PENNY_WORTH = 0.01, DOLLAR_WORTH = 1.0, FIVE_DOLLAR_WORTH = 5.0, TEN_DOLLAR_WORTH = 10.0;
+    double fWorth;
+
+    public Conversions()
+    {
+
+    }
+
+    public void InsertCoins(int iQ, int iD, int iN, int iP)
+    {
+
+    }
+
+    private void CalculateWorth()
+    {
+
+    }
+}
--- src/MoneyConverter.java
+++ src/MoneyConverter.java
@@ -3,11 +3,38 @@
  * @since 3 October 2017
  */
 
+import java.util.Scanner;
+
 public class MoneyConverter {
-    static int iQuarters, iDimes, iNickels, iPennies;
+    private static int iQuarters, iDimes, iNickels, iPennies;
+    private final static Scanner SCAN = new Scanner(System.in);
 
     public static void main(String[] args)
     {
+        iQuarters = PromptInput("Number of quarters: ");
+        iDimes = PromptInput("Number of dimes: ");
+        iNickels = PromptInput("Number of nickels: ");
+        iPennies = PromptInput("Number of pennies: ");
+    }
 
+    /**
+     * Prompt user to enter an integer based on the question, and loop until integer only.
+     * @param {Buffer} question
+     * @return {Integer} User input
+     */
+    private static int PromptInput(String question)
+    {
+        System.out.print(question);
+
+        while (!SCAN.hasNextInt())
+        {
+            System.out.flush();
+
+            System.out.print("Whole number only: ");
+
+            SCAN.next();
+        }
+
+        return SCAN.nextInt();
     }
 }
